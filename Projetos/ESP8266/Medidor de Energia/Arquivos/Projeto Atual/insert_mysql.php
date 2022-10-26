55c47e294fcb3d6772ec717109e8e93933f3876d
diegopnf <36850563+diegopnf@users.noreply.github.com>
2022-10-25 21:31:00 -0300

    Inserir dados no Mysql

---
+++ Projetos/ESP8266/Medidor de Energia/Arquivos/Projeto Atual/insert_mysql.php
@@ -0,0 +1,28 @@
+<?php
+$codigo = filter_input(INPUT_GET, 'codigo'); //variaveis da firmware esp8266
+$tensao = filter_input(INPUT_GET, 'tensao'); //variaveis da firmware esp8266
+$corrente = filter_input(INPUT_GET, 'corrente'); //variaveis da firmware esp8266
+$potencia = filter_input(INPUT_GET, 'potencia'); //variaveis da firmware esp8266
+$potenciahora = filter_input(INPUT_GET, 'potenciahora'); //variaveis da firmware esp8266
+$frequencia = filter_input(INPUT_GET, 'frequencia'); //variaveis da firmware esp8266
+$fatordepotencia = filter_input(INPUT_GET, 'fatordepotencia'); //variaveis da firmware esp8266
+
+
+
+$servername = "localhost";
+$username = "XXXXXXXXXX"; //usuario do banco
+$password = "XXXXXXXXXX"; //senha do banco
+$dbname = "XXXXXXXXXX"; // nome do banco
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  //Gravar log de erros
+  die("Não foi possível estabelecer conexão com o BD: " . $conn->connect_error);
+}
+$sql = "INSERT INTO medidor_de_energia.equipamento (codigo, tensao, corrente, potencia, potenciahora, frequencia, fatordepotencia) VALUES ($codigo,$tensao,$corrente,$potencia,$potenciahora,>
+
+if (!$conn->query($sql)) {
+  //Gravar log de erros
+  die("Erro na gravação dos dados no BD");
+}
+$conn->close();
+?>
